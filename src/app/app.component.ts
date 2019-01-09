b244f2b95e3413293c198314c0ca2e5696aa3798
SabikAbtahee <bsse0829@iit.du.ac.bd>
2019-01-09 16:16:19 +0600

    1/9/2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'v1';
+  title = 'Theme Assignment';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import  {FlexLayoutModule} from '@angular/flex-layout';
+import { MatToolbarModule, MatSidenavModule, MatCardModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MainContentComponent } from './main-content/main-content.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    MainContentComponent,
+   
+  
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatListModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
